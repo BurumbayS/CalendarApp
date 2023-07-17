446a5d5ef0ae140633db7c07d6ee2474bb660e91
BurumbayS <sanzharbr@gmail.com>
2023-07-17 22:43:34 +0600

    Created CalendarView with sample event cells

--- CalendarApp/CalendarViewController.swift
+++ CalendarApp/CalendarViewController.swift
@@ -6,8 +6,60 @@
 //
 
 import UIKit
-
+import SnapKit
 
 final class CalendarViewController: UIViewController {
     
+    private var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+    }
+    
+    private func setupViews() {
+        let calendarView = UICalendarView()
+        calendarView.calendar = Calendar(identifier: .gregorian)
+        calendarView.delegate = self
+        calendarView.selectionBehavior = UICalendarSelectionSingleDate(delegate: self)
+        view.addSubview(calendarView)
+        calendarView.snp.makeConstraints { view in
+            view.leading.trailing.top.equalToSuperview()
+        }
+        
+        tableView = UITableView(frame: .zero, style: .plain)
+        tableView.dataSource = self
+        tableView.register(EventItemCell.self, forCellReuseIdentifier: String(describing: EventItemCell.self))
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints { view in
+            view.leading.trailing.bottom.equalToSuperview()
+            view.top.equalTo(calendarView.snp.bottom).offset(10)
+        }
+    }
+}
+
+extension CalendarViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: EventItemCell.self), for: indexPath) as? EventItemCell else {
+            return UITableViewCell()
+        }
+        
+        cell.configure(title: "Hello", time: "15:23")
+        return cell
+    }
+}
+
+extension CalendarViewController: UICalendarViewDelegate, UICalendarSelectionSingleDateDelegate {
+    
+    func calendarView(_ calendarView: UICalendarView, decorationFor dateComponents: DateComponents) -> UICalendarView.Decoration? {
+        return .default(color: .blue, size: .medium)
+    }
+    
+    func dateSelection(_ selection: UICalendarSelectionSingleDate, didSelectDate dateComponents: DateComponents?) {
+        
+    }
 }
---
+++ CalendarApp/EventItemCell.swift
@@ -0,0 +1,40 @@
+//
+//  EventItemCell.swift
+//  CalendarApp
+//
+//  Created by Sanzhar Burumbay on 17.07.2023.
+//
+
+import UIKit
+
+final class EventItemCell: UITableViewCell {
+    
+    private let titleLabel = UILabel()
+    private let timeLabel = UILabel()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(title: String, time: String) {
+        titleLabel.text = title
+        timeLabel.text = time
+    }
+    
+    private func setupViews() {
+        contentView.addSubview(titleLabel)
+        titleLabel.snp.makeConstraints { view in
+            view.leading.top.bottom.equalToSuperview().inset(16)
+        }
+        contentView.addSubview(timeLabel)
+        timeLabel.snp.makeConstraints { view in
+            view.trailing.top.bottom.equalToSuperview().inset(16)
+            view.leading.equalTo(titleLabel.snp.trailing).offset(10)
+        }
+    }
+}
